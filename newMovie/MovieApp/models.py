92c3f60e96f4e3c382311a1fa82947eb28ea8b11
Dantheman3345 <u1022647@utah.edu>
2024-03-25 15:02:06 -0600

    With new Models

--- newMovie/MovieApp/models.py
+++ newMovie/MovieApp/models.py
@@ -13,4 +13,45 @@ class Movie(models.Model):
     genre = models.CharField(max_length=50, blank=True)
     image_url = models.URLField(blank=True)
     youtube_url = models.URLField(blank=True)
-# Create your models here.
+
+
+class MovieCharacter(models.Model):
+    character_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Award(models.Model):
+    award_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Studio(models.Model):
+    studio_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    founded = models.DateField()
+    owner = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Actor(models.Model):
+    actor_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Director(models.Model):
+    director_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
